cb57893f68bd03664ae814f46f2e4ce87a2f2046
Angelo Bonini <angelo.bonini@institutosoma.org.br>
2021-06-15 21:16:20 -0300

    starting on developing the game

--- Jogo/JogoBase.java
+++ Jogo/JogoBase.java
@@ -6,15 +6,22 @@ import java.io.*;
 import javax.imageio.*;
 
 class JogoBase extends JFrame {
-  Image img;
+  Image img[] = new Image[8] ;
   Desenho des = new Desenho();
 
   class Desenho extends JPanel {
 
     Desenho() {
       try {
-        setPreferredSize(new Dimension(1000, 600));
-        img = ImageIO.read(new File("fundo.jpeg"));
+        setPreferredSize(new Dimension(700, 500));
+        img[0] = ImageIO.read(new File("fundo.jpeg"));
+        img[1] = ImageIO.read(new File("parado.gif"));
+        img[2] = ImageIO.read(new File("anda0.gif"));
+        img[3] = ImageIO.read(new File("anda1.gif"));
+        img[4] = ImageIO.read(new File("soco0.gif"));
+        img[5] = ImageIO.read(new File("soco1.gif"));
+        img[6] = ImageIO.read(new File("soco2.gif"));
+        img[7] = ImageIO.read(new File("arbusto.png"));
       } catch (IOException e) {
         JOptionPane.showMessageDialog(this, "A imagem não pode ser carregada!\n" + e, "Erro", JOptionPane.ERROR_MESSAGE);
         System.exit(1);
@@ -23,7 +30,7 @@ class JogoBase extends JFrame {
 
     public void paintComponent(Graphics g) {
       super.paintComponent(g);
-      g.drawImage(img, 0, 0, getSize().width, getSize().height, this);
+      g.drawImage(img[0], 0, 0, getSize().width, getSize().height, this);
       Toolkit.getDefaultToolkit().sync();
     }
   }
