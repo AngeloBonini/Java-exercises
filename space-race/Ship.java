8015f894828c0ae8c131ba9d8fff8e81191b6d7b
Angelo Bonini <angelo.bonini@institutosoma.org.br>
2021-06-23 17:23:17 -0300

    starting the game classes

--- space-race/Ship.java
+++ space-race/Ship.java
@@ -1,25 +1,32 @@
 import java.awt.Graphics;
 
 public class Ship {
+    Image ship = new Image[1];
+int x;
+int y;
+int score;
+int raio;
+boolean acima;
+boolean abaixo;
 
-    constructor(x) {
-        this.x = x;
+    public Ship() {
+        this.x = 120;
         this.score = 0;
         this.respawn();
         this.raio = 10;
-
+        ship[0] = ImageIO.read(new File("pixil-frame-0.png"));
     }
 
-    respawn(){
-        this.y = height - 20;
+    public void respawn(){
+        this.y = 880;
         this.acima = false;
         this.abaixo = false;
     }
 
-    atualiza(){
+    public void  atualiza(){
         if(this.acima && this.y > 0){
             this.sobe();
-        } else if(this.abaixo && this.y < height -20 ){
+        } else if(this.abaixo && this.y < 880 ){
             this.desce();
         }
         if(this.jogadorFezPonto()){
@@ -28,24 +35,27 @@ public class Ship {
         }
     }
 
-    public void display(Graphics page) {
-        page.setColor(Color.white);
-        page.fillOval(this.x, this.y, this.raio*2,this.raio*2 );
-    }
 
-    sobe(){
+    public void sobe(){
         this.y++;
     }
 
-    desce(){
+    public void desce(){
         this.y--;
     }
 
-    jogadorFezPonto(){
+    public boolean jogadorFezPonto(){
         if(this.y <=0){
             return true;
         }
         return false;
-    }    
-
+    }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(img[0],  30, 60, this);
+        Toolkit.getDefaultToolkit().sync();
+      }    
+public static void main(String[] args){
+    Ship nave = new Ship();
+}
 }
