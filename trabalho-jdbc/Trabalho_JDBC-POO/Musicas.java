a7e70606c6ba88d82fd19fee0a0a232c7cdc37d1
Angelo Bonini <abonini2000@gmail.com>
2021-08-01 13:32:35 -0300

    creating the songs table

--- trabalho-jdbc/Trabalho_JDBC-POO/Musicas.java
+++ trabalho-jdbc/Trabalho_JDBC-POO/Musicas.java
@@ -20,14 +20,14 @@ import java.sql.ResultSet;
 public class Musicas extends Janela {
     JPanel Campos[] = new JPanel[4];
     JTable Tabela;
-    JTextArea InsCliente, InsValor, InsDescricao, AltID, AltCliente, AltValor, AltDescricao, ExcID;
+    JTextArea InsTitulo, InsDuracao, InsLetra, AltID, AltTitulo, AltDuracao, AltLetra, ExcID;
     JButton BInserir, BAlterar, BExcluir;
 
     Musicas() {
         super("Musicas");
         CriaBancoDados("BD-Musicas");
         try {
-            Afirmacao.executeUpdate("CREATE TABLE MUSICAS (TITULO VARCHAR(50), DURACAO FLOAT, LETRA VARCHAR(100))");
+            Afirmacao.executeUpdate("CREATE TABLE MUSICAS (TITULO VARCHAR(50), DURACAO FLOAT, LETRA VARCHAR(500))");
         } catch (SQLException excecao) {}
 
         Border borda = BorderFactory.createLineBorder(Color.black);
@@ -35,56 +35,56 @@ public class Musicas extends Janela {
         for (int i = 0; i < 4; i++) Campos[i] = new JPanel();
 
         Campos[0].setLayout(new GridLayout(4, 2));
-        Campos[0].add(new JLabel("Cadastrar pedido"));
+        Campos[0].add(new JLabel("Cadastrar musica"));
         BInserir = new JButton("Cadastrar");
         BInserir.addActionListener(new Insere());
         Campos[0].add(BInserir);
-        Campos[0].add(new JLabel("Cliente"));
-        InsCliente = new JTextArea();
-        InsCliente.setBorder(borda);
-        Campos[0].add(InsCliente);
-        Campos[0].add(new JLabel("Valor"));
-        InsValor = new JTextArea();
-        InsValor.setBorder(borda);
-        Campos[0].add(InsValor);
-        Campos[0].add(new JLabel("Descricao"));
-        InsDescricao = new JTextArea();
-        InsDescricao.setBorder(borda);
-        Campos[0].add(InsDescricao);
+        Campos[0].add(new JLabel("Título"));
+        InsTitulo = new JTextArea();
+        InsTitulo.setBorder(borda);
+        Campos[0].add(InsTitulo);
+        Campos[0].add(new JLabel("Duração"));
+        InsDuracao = new JTextArea();
+        InsDuracao.setBorder(borda);
+        Campos[0].add(InsDuracao);
+        Campos[0].add(new JLabel("Letra"));
+        InsLetra = new JTextArea();
+        InsLetra.setBorder(borda);
+        Campos[0].add(InsLetra);
 
         Campos[1].setLayout(new GridLayout(5, 2));
-        Campos[1].add(new JLabel("Alterar pedido"));
+        Campos[1].add(new JLabel("Alterar dados da música"));
         BAlterar = new JButton("Alterar");
         BAlterar.addActionListener(new Altera());
         Campos[1].add(BAlterar);
-        Campos[1].add(new JLabel("Nome do cliente cujo pedido sera alterado"));
+        Campos[1].add(new JLabel("Título da música a ser alterada"));
         AltID = new JTextArea();
         AltID.setBorder(borda);
         Campos[1].add(AltID);
-        Campos[1].add(new JLabel("Cliente"));
-        AltCliente = new JTextArea();
-        AltCliente.setBorder(borda);
-        Campos[1].add(AltCliente);
-        Campos[1].add(new JLabel("Valor"));
-        AltValor = new JTextArea();
-        AltValor.setBorder(borda);
-        Campos[1].add(AltValor);
-        Campos[1].add(new JLabel("Descricao"));
-        AltDescricao = new JTextArea();
-        AltDescricao.setBorder(borda);
-        Campos[1].add(AltDescricao);
+        Campos[1].add(new JLabel("Novo Título"));
+        AltTitulo = new JTextArea();
+        AltTitulo.setBorder(borda);
+        Campos[1].add(AltTitulo);
+        Campos[1].add(new JLabel("Nova duração"));
+        AltDuracao = new JTextArea();
+        AltDuracao.setBorder(borda);
+        Campos[1].add(AltDuracao);
+        Campos[1].add(new JLabel("Nova Letra"));
+        AltLetra = new JTextArea();
+        AltLetra.setBorder(borda);
+        Campos[1].add(AltLetra);
 
         Campos[2].setLayout(new GridLayout(2, 2));
-        Campos[2].add(new JLabel("Excluir cadastro"));
+        Campos[2].add(new JLabel("Excluir Excluir música"));
         BExcluir = new JButton("Excluir");
         BExcluir.addActionListener(new Exclui());
         Campos[2].add(BExcluir);
-        Campos[2].add(new JLabel("Nome do cliente cujo pedido sera excluido"));
+        Campos[2].add(new JLabel("Nome do nome da música excluida"));
         ExcID = new JTextArea();
         ExcID.setBorder(borda);
         Campos[2].add(ExcID);
 
-        Tabela = new JTable(new Object[1][4], new String[] {"Cliente", "Valor", "Descricao"});
+        Tabela = new JTable(new Object[1][4], new String[] {"Titulo", "Duração", "Letra"});
         Campos[3].setLayout(new GridLayout(2, 1));
         Campos[3].add(Tabela.getTableHeader());
         Campos[3].add(Tabela);
@@ -94,14 +94,14 @@ public class Musicas extends Janela {
         CriaJanela(Campos);
     }
     public void AtualizaJanela() {
-        DefaultTableModel Modelo = new DefaultTableModel(new Object[] {"Cliente", "Valor", "Descricao"}, 0);
+        DefaultTableModel Modelo = new DefaultTableModel(new Object[] {"Título", "Duração", "Letra"}, 0);
         try {
             ResultSet Dados = Afirmacao.executeQuery("SELECT * FROM MUSICAS");
             while (Dados.next()) {
-                String Cliente = Dados.getString("TITULO");
-                float Valor = Dados.getFloat("DURACAO");
-                String Descricao = Dados.getString("LETRA");
-                Modelo.addRow(new Object[] {Cliente, Valor, Descricao});
+                String Titulo = Dados.getString("TITULO");
+                float Duracao = Dados.getFloat("DURACAO");
+                String Letra = Dados.getString("LETRA");
+                Modelo.addRow(new Object[] {Titulo, Duracao, Letra});
                 Dados.next();
             }
         } catch (SQLException erro) {
@@ -112,38 +112,38 @@ public class Musicas extends Janela {
     }
     class Insere implements ActionListener {
         public void actionPerformed(ActionEvent evento) {
-            String Cliente = InsCliente.getText();
-            String valor = InsValor.getText();
-            String Descricao = InsDescricao.getText();
-            if (Cliente.isEmpty()) {
+            String Titulo = InsTitulo.getText();
+            String Duracao = InsDuracao.getText();
+            String Letra = InsLetra.getText();
+            if (Titulo.isEmpty()) {
                 JOptionPane.showMessageDialog(null, "Insira um cliente.");
                 return;
             }
-            if (Cliente.length() > 50) {
+            if (Titulo.length() > 50) {
                 JOptionPane.showMessageDialog(null, "Insira um nome com 50 caracteres ou menos.");
                 return;
             }
-            if (valor.isEmpty()) {
-                JOptionPane.showMessageDialog(null,"Insira um valor.");
+            if (Duracao.isEmpty()) {
+                JOptionPane.showMessageDialog(null,"Insira um Duracao.");
                 return;
             }
-            float Valor;
-            try {
-                Valor = Float.parseFloat(valor);
-            } catch (NumberFormatException erro) {
-                JOptionPane.showMessageDialog(null,"Insira um numero real como valor.");
-                return;
-            }
-            if (Descricao.isEmpty()) {
+            // float Duracao;
+            // try {
+            //     Duracao = Float.parseFloat(Duracao);
+            // } catch (NumberFormatException erro) {
+            //     JOptionPane.showMessageDialog(null,"Insira um numero real como Duracao.");
+            //     return;
+            // }
+            if (Letra.isEmpty()) {
                 JOptionPane.showMessageDialog(null, "Insira uma descricao.");
                 return;
             }
-            if (Descricao.length() > 100) {
+            if (Letra.length() > 100) {
                 JOptionPane.showMessageDialog(null, "Insira uma descricao com 100 caracteres ou menos.");
                 return;
             }
             try {
-                Afirmacao.executeUpdate("INSERT INTO MUSICAS VALUES('"+ Cliente +"', "+ Valor +", '"+ Descricao +"')");
+                Afirmacao.executeUpdate("INSERT INTO MUSICAS VALUES('"+ Titulo +"', '"+ Duracao +"', '"+ Letra +"')");
             } catch(SQLException erro) {
                 erro.printStackTrace();
                 System.exit(1);
@@ -154,9 +154,9 @@ public class Musicas extends Janela {
     class Altera implements ActionListener {
         public void actionPerformed(ActionEvent evento) {
             String ClienteEspecificado = AltID.getText();
-            String Cliente = AltCliente.getText();
-            String valor = AltValor.getText();
-            String Descricao = AltDescricao.getText();
+            String Titulo = AltTitulo.getText();
+            String Duracao = AltDuracao.getText();
+            String Letra = AltLetra.getText();
             if (ClienteEspecificado.isEmpty()) {
                 JOptionPane.showMessageDialog(null, "Insira um nome como especificacao.");
                 return;
@@ -165,35 +165,35 @@ public class Musicas extends Janela {
                 JOptionPane.showMessageDialog(null, "Insira um nome com 50 caracteres ou menos como especificacao.");
                 return;
             }
-            if (Cliente.isEmpty()) {
+            if (Titulo.isEmpty()) {
                 JOptionPane.showMessageDialog(null, "Insira um cliente.");
                 return;
             }
-            if (Cliente.length() > 50) {
+            if (Titulo.length() > 50) {
                 JOptionPane.showMessageDialog(null, "Insira um nome com 50 caracteres ou menos.");
                 return;
             }
-            if (valor.isEmpty()) {
-                JOptionPane.showMessageDialog(null,"Insira um valor.");
+            if (Duracao.isEmpty()) {
+                JOptionPane.showMessageDialog(null,"Insira um Duracao.");
                 return;
             }
-            float Valor;
-            try {
-                Valor = Float.parseFloat(valor);
-            } catch (NumberFormatException erro) {
-                JOptionPane.showMessageDialog(null,"Insira um numero real como valor.");
-                return;
-            }
-            if (Descricao.isEmpty()) {
+            // float Duracao;
+            // try {
+            //     Duracao = Float.parseFloat(Duracao);
+            // } catch (NumberFormatException erro) {
+            //     JOptionPane.showMessageDialog(null,"Insira um numero real como Duracao.");
+            //     return;
+            // }
+            if (Letra.isEmpty()) {
                 JOptionPane.showMessageDialog(null, "Insira uma descricao.");
                 return;
             }
-            if (Descricao.length() > 100) {
+            if (Letra.length() > 100) {
                 JOptionPane.showMessageDialog(null, "Insira uma descricao com 100 caracteres ou menos.");
                 return;
             }
             try {
-                Afirmacao.executeUpdate("UPDATE MUSICAS SET TITULO='"+ Cliente +"', DURACAO='"+ Valor +"', LETRA='"+ Descricao +"' WHERE TITULO='"+ ClienteEspecificado +"'");
+                Afirmacao.executeUpdate("UPDATE MUSICAS SET TITULO='"+ Titulo +"', DURACAO='"+ Duracao +"', LETRA='"+ Letra +"' WHERE TITULO='"+ ClienteEspecificado +"'");
             } catch(SQLException erro) {
                 erro.printStackTrace();
                 System.exit(1);
@@ -203,17 +203,17 @@ public class Musicas extends Janela {
     }
     class Exclui implements ActionListener {
         public void actionPerformed(ActionEvent evento) {
-            String Cliente = ExcID.getText();
-            if (Cliente.isEmpty()) {
+            String Titulo = ExcID.getText();
+            if (Titulo.isEmpty()) {
                 JOptionPane.showMessageDialog(null,"Insira um nome como especificacao.");
                 return;
             }
-            if (Cliente.length() > 50) {
+            if (Titulo.length() > 50) {
                 JOptionPane.showMessageDialog(null,"Insira um nome com 50 ou menos caracteres como especificacao.");
                 return;
             }
             try {
-                Afirmacao.executeUpdate("DELETE FROM MUSICAS WHERE TITULO='"+ Cliente +"'");
+                Afirmacao.executeUpdate("DELETE FROM MUSICAS WHERE TITULO='"+ Titulo +"'");
             } catch(SQLException erro) {
                 erro.printStackTrace();
                 System.exit(1);
